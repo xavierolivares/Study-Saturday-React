dd7c4c0b29819b79afe259f2bc4ffc043c0ed40f
Xavier Olivares <xm.olivares@gmail.com>
2019-05-04 12:05:46 -0400

    react work

---
+++ browser/components/Main.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+//how do i gather data from the database?
+
+export default class Main extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            students: {}
+        }
+    }
+
+    async componentDidMount() {
+        //{data} was initially an object
+        //I needed to destructure it in order to pull the actual data I needed
+        
+        const {data}= await axios.get('/student')
+        console.log('data is ', data)
+        this.setState({
+            students: data
+        })
+        // paused on promise rejection with status 404
+
+
+        // console.log(data)
+        // practice using axios to get the students
+        //console.log what i'm receiving from the db
+    }
+    render() {
+    console.log('state is ', this.state)
+    return (
+        <h1>{'Hello React!'}</h1>
+
+    );
+    }
+    
+}
--- browser/index.js
+++ browser/index.js
@@ -1,1 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Main from './components/Main'
+
 console.log('Hello Webpack!');
+
+ReactDOM.render(<Main />, document.getElementById('app'))
